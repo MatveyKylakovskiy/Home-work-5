173a411e4352a6cba00629211384cbfadf8908a2
Matveykylak <matveykylak@gmail.com>
2024-03-30 16:15:31 +0300

    First task

--- Home work 5/Program.cs
+++ Home work 5/Program.cs
@@ -1,1 +1,25 @@
-﻿//
+﻿//Firs task
+
+/*using System.Text;
+
+
+StringBuilder str = new StringBuilder("13 test this solution test 13 ");
+
+str = str.Replace("test", "testing");
+
+var str1 = str.ToString();
+
+
+for (var i = 0; i < str1.Length - 1; i++)
+{
+    if (char.IsDigit(str1[i]))
+    {
+        str1 = str1.Replace($"{str1[i]}", "");
+
+    }
+}
+
+Console.WriteLine(str1);
+*/
+
+

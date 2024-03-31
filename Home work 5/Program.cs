aaeeb0fd24bda4bef6a77cf7e1ce302c75485a9a
Matveykylak <matveykylak@gmail.com>
2024-03-31 11:23:33 +0300

    Third task

--- Home work 5/Program.cs
+++ Home work 5/Program.cs
@@ -24,13 +24,35 @@ Console.WriteLine(str1);
 
 //Second task
 
-var s1 = "Welcome";
+/*var s1 = "Welcome";
 var s2 = "to";
 var s3 = "the";
 var s4 = "TMS";
 var s5 = "lesons";
 
 Console.WriteLine(string.Concat(s1, " " , s2, " " , s3," " , s4, " " , s5));
+*/
+
+//Third task
+
+var check = "abc";
+var str = "teamwithsomeofexcersicesabcwanttomakeitbetter";
+
+var firstIndex = str.IndexOf(check);
+var secondIndex = str.LastIndexOf(check);
+
+var str1 = str.Substring(0, firstIndex);
+
+var index1 = secondIndex + check.Length;
+var index2 = str.Length - index1;
+
+var str2 = str.Substring(index1,index2 );
+
+Console.WriteLine("Expected result:      teamwithsomeofexcersices");
+Console.WriteLine($"The result obtained:  {str1}");
+
+Console.WriteLine("Expected result:      wanttomakeitbetter");
+Console.WriteLine($"The result obtained:  {str2}");
 
 
 
